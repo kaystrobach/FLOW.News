8fd6f16bb62c0f646f0ea0c88fa42e0c866fb2e3
Kay Strobach <info@kay-strobach.de>
2015-06-22 16:24:22 +0200

    [TASK] add controller

---
+++ Classes/KayStrobach/News/Controller/NewsController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace KayStrobach\News\Controller;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "SBS.LaPo".              *
+ *                                                                        *
+ *                                                                        */
+
+use KayStrobach\News\Domain\Model\News;
+use TYPO3\Flow\Annotations as Flow;
+
+class NewsController extends \TYPO3\Flow\Mvc\Controller\ActionController {
+	/**
+	 * @Flow\Inject
+	 * @var \KayStrobach\News\Domain\Repository\NewsRepository
+	 */
+	protected $newsRepository;
+
+	/**
+	 * Wird vor allen actions aufgerufen
+	 */
+	public function initializeAction() {
+
+	}
+
+	/**
+	 * Liste aller eingetragenen Nachrichten anzeigen
+	 * @return void
+	 */
+	public function indexAction() {
+		$this->view->assign('news', $this->newsRepository->findAll());
+	}
+
+	/**
+	 * Anlegen einer neuen Nachricht durch Verwaltung
+	 * @return void
+	 */
+	public function newAction() {
+		$news = new News();
+		$news->setStartDate(new \DateTime('now'));
+		$news->setEndDate(new \DateTime('+3month'));
+		$this->view->assign('news', $news);
+	}
+
+	/**
+
+	 * @param News $news
+	 */
+	public function editAction(News $news) {
+		$this->view->assign('news', $news);
+	}
+
+	/**
+	 * @param News $news
+	 * @return void
+	 */
+	public function showAction(News $news) {
+		$this->view->assign('news', $news);
+	}
+
+	/**
+	 * @Flow\ValidationGroups({"NewsSubject","NewsContent"})
+	 * @param News $news
+	 */
+	public function updateAction(News $news) {
+		$this->newsRepository->update($news);
+		$this->redirect('index');
+	}
+
+	/**
+	 * @Flow\ValidationGroups({"NewsSubject","NewsContent"})
+	 * @param News $news
+	 */
+	public function createAction(News $news) {
+		$this->newsRepository->add($news);
+		$this->redirect('index');
+	}
+
+	/**
+	 * Löschen der Nachricht durch Verwaltung
+	 * @param News $news
+	 * @return void
+	 */
+	public function deleteAction(News $news) {
+		$this->newsRepository->remove($news);
+		$this->addFlashMessage('Die Nachricht wurde gelöscht.');
+		$this->redirect('index');
+	}
+}
